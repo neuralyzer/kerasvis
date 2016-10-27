754c1341057675756e5ca892c2d8599221cc64bf
Vit Listik <vit.listik@firma.seznam.cz>
2016-10-27 10:24:17 +0200

    do not use zip

--- setup.py
+++ setup.py
@@ -12,4 +12,5 @@ setup(name="kerasvis",
     keywords="keras optimization visualization",
     install_requires=["flask", "bokeh", "pandas"],
     packages=find_packages(),
-    include_package_data = True)
+    include_package_data = True,
+    zip_safe = False)
